eb5b5e5a574008f3de0e18e0a0d262c2de7f4530
SpyerNot <low_li_wen@students.edu.sg>
2024-09-22 18:13:43 +0800

    Update home.py

--- home.py
+++ home.py
@@ -6,6 +6,9 @@ if page == "About me":
   st.subheader("Welcome to the about me page")
 elif page == "Achievements":
   st.subheader("Welcome to the achievements page")
+  col1, col2, col3 = st.columns(3)
+  with col1:
+    st.image("https://drive.google.com/drive/u/0/home")
 elif page == "Experiences":
   st.subheader("Welcome to the experiences page")
 else:
