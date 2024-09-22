d5eda26eb70826f200cb8d6e0f1af124f632a0d4
SpyerNot <low_li_wen@students.edu.sg>
2024-09-22 17:45:19 +0800

    Update home.py

--- home.py
+++ home.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("Low Li Wen's E-portfolio")
-pg = st.navigaton([st.Page("about_me.py",title="Info about me"),st.Page("achievements.py",title="All my achievements in one place :D"),st.Page("experiences.py",title="My different experiences"),st.Page("projects.py",title="My coding projects")])
+pg = st.navigation([st.Page("about_me.py",title="Info about me"),st.Page("achievements.py",title="All my achievements in one place :D"),st.Page("experiences.py",title="My different experiences"),st.Page("projects.py",title="My coding projects")])
