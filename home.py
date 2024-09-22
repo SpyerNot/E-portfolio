1c761296a6e8f6da2598d259de2e1bf7a0b2e73c
SpyerNot <low_li_wen@students.edu.sg>
2024-09-22 18:08:37 +0800

    Update home.py

--- home.py
+++ home.py
@@ -3,11 +3,11 @@ st.title("Low Li Wen's E-portfolio")
 st.sidebar.title("Sidebar")
 page = st.sidebar.radio("Go to", ["About me","Achievements","Experiences","Projects"])
 if page == "About me":
-  st.subheader("Welcome to the \"about me"\ page")
+  st.subheader("Welcome to the about me page")
 elif page == "Achievements":
-  st.subheader("Welcome to the \"achievements"\ page")
+  st.subheader("Welcome to the achievements page")
 elif page == "Experiences":
-  st.subheader("Welcome to the \"experiences"\ page")
+  st.subheader("Welcome to the experiences page")
 else:
-  st.subheader("Welcome to the \"projects"\ page")
+  st.subheader("Welcome to the projects page")
 
