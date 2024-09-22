23b0c43d883a48adbb92e9d8b4af73fee7c7e53a
SpyerNot <low_li_wen@students.edu.sg>
2024-09-22 18:19:36 +0800

    Update home.py

--- home.py
+++ home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 st.title("Low Li Wen's E-portfolio")
 st.sidebar.title("Sidebar")
 page = st.sidebar.radio("Go to", ["About me","Achievements","Experiences","Projects"])
@@ -8,7 +9,8 @@ elif page == "Achievements":
   st.subheader("Welcome to the achievements page")
   col1, col2, col3 = st.columns(3)
   with col1:
-    st.image("https://drive.google.com/file/d/1w_-hWtGOEDcUNGMx70Mot8E3eolxe9zW/view?usp=sharing")
+    response = requests.get("https://drive.google.com/file/d/1w_-hWtGOEDcUNGMx70Mot8E3eolxe9zW/view?usp=sharing")
+    st.image(response.content)
   with col2:
 elif page == "Experiences":
   st.subheader("Welcome to the experiences page")
